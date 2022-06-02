269daf2ac7406329bd84072e4a57035bd256bd29
Priyanka-koraganji <51729374+Priyanka-koraganji@users.noreply.github.com>
2022-06-02 13:48:35 -0700

    Update index.js
    modified the file by adding middleware functions and http routes

--- index.js
+++ index.js
@@ -1,16 +1,66 @@
 const express = require('express'),
-morgan = require('morgan');
+  morgan = require('morgan');
 
 const app = express();
+app.use(express.static('public'));
+
 app.use(morgan('common'));
 
+let movies = [
+  {
+    title: 'The Godfather',
+    year: 1972
+  },
+  {
+    title: ' The Shawshank Redemption',
+    year: 1994
+  },
+  {
+    title: 'Schindlers List',
+    year: 1993
+  },
+  {
+    title: 'Raging Bull',
+    year: 1980
+  },
+  {
+    title: 'Casablanca',
+    year: 1942
+  },
+  {
+    title: ' Citizen Kane',
+    year: 1941
+  },
+  {
+    title: 'Gone with the Wind',
+    year: 1939
+  },
+  {
+    title: 'The Wizard of Oz ',
+    year: 1939
+  },
+  {
+    title: 'One Flew Over the Cuckoos Nest ',
+    year: 1975
+  },
+  {
+    title: ' Lawrence of Arabia',
+    year: 1962
+  },
+
+]
 
 app.get('/', (req, res) => {
   res.send('Welcome to my app!');
 });
 
-app.get('/secreturl', (req, res) => {
-  res.send('This is a secret url with super top-secret content.');
+app.get('/movies', (req, res) => {
+  res.json(movies);
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
 });
 
 app.listen(8080, () => {
