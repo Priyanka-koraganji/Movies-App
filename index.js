d6e20041fb2a90c9889bba250296561dd275e2ff
Priyanka-koraganji <51729374+Priyanka-koraganji@users.noreply.github.com>
2022-06-14 23:23:30 -0700

    Update index.js
    connected heroku

--- index.js
+++ index.js
@@ -15,9 +15,10 @@ const Models = require('./models.js');
 
 const Movies = Models.Movie;
 const Users = Models.User;
-mongoose.connect('mongodb://0.0.0.0:27017/myFlixDB', { useNewUrlParser: true, useUnifiedTopology: true });
+//mongoose.connect('mongodb://0.0.0.0:27017/myFlixDB', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.CONNECTION_URI,{ useNewUrlParser: true, useUnifiedTopology: true });
 
-//middleware
+  //middleware
 app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
