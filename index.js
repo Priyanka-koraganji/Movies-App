2273975a6783484d4bc60e5aa05f6685b77ecfed
Priyanka-koraganji <51729374+Priyanka-koraganji@users.noreply.github.com>
2022-07-01 18:59:19 -0700

    Update index.js
    added cors

--- index.js
+++ index.js
@@ -10,7 +10,20 @@ const app = express();
 
 //cors
 const cors = require('cors');
-app.use(cors());
+//app.use(cors());
+let allowedOrigins = ['http://localhost:8080', 'http://localhost:1234/','https://yourmoviescollection.herokuapp.com/'];
+
+
+app.use(cors({
+  origin: (origin, callback) => {
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
+      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+      return callback(new Error(message ), false);
+    }
+    return callback(null, true);
+  }
+}));
 
 //using mongoose Models
 const mongoose = require('mongoose');
